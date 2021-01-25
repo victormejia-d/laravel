fec0833acb8b0cdf874cb2474486f41012403417
Victor Mejia <vmejia@bluepeople.com>
2021-01-25 02:31:56 -0600

    Modify default language

--- resources/views/datos.blade.php
+++ resources/views/datos.blade.php
@@ -11,7 +11,7 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
     crossorigin="anonymous"
   >
-  <title>Página de contacto</title>
+  <title>Victor - Página de contacto</title>
 </head>
 
 <body>
